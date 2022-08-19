f4b3fc0eb9dde5357e49f3fea85fb2e5e872601e
ltizzi <terlizzileonardo@gmail.com>
2022-08-19 19:50:24 -0300

    module about modules about to get finished

--- 1- node modules/module-example/https.js
+++ 1- node modules/module-example/https.js
@@ -1,13 +1,18 @@
-const { send } = require("./request"); //asi se importan modulos en node
-const { read } = require("./response");
+// const { send } = require("./internals/request"); //asi se importan modulos en node
+// const { read } = require("./internals/response");
+const internals = require("./internals"); //se importa una carpeta entera a través de index.js
 
 //con const {send } puedo evitar el request.send en la funcion de abajo
 
 function makeRequest(url, data) {
   //request.send(url, data);
-  send(url, data);
-  return read();
+  // send(url, data);
+  // return read();
+  internals.request.send(url, data);
+  return internals.response.read();
 }
 
 const responseData = makeRequest("https://google.com", "hi");
 console.log(responseData);
+
+//para usar ECMASCript modules hay que cambiar las extension a .mjs
