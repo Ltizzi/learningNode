aedf747e51c76ec819420fe3e35231ff76bcaf3b
ltizzi <terlizzileonardo@gmail.com>
2022-10-27 19:16:34 -0300

    ended with express module

--- 4 - Express/controllers/messages.controller.js
+++ 4 - Express/controllers/messages.controller.js
@@ -3,9 +3,13 @@ const path = require("path");
 function getMessages(req, res) {
   //res.send("<ul><li>helloooo Albert!!</li></ul>");
   // dirname=dir actual, .. para atras, carpeta public etc
-  res.sendFile(
-    path.join(__dirname, "..", "public", "images", "skimountain.jpg")
-  );
+  // res.sendFile(    path.join(__dirname, "..", "public", "images", "skimountain.jpg")  );
+
+  //render template en el endpoint
+  res.render("messages", {
+    tittle: "Messages to my Friends",
+    friend: 'Elon "El Marico" Musk',
+  });
 }
 
 function postMsg(req, res) {
--- 4 - Express/server.js
+++ 4 - Express/server.js
@@ -1,4 +1,4 @@
-//QUINTA VERSION - serving static website
+//QUINTA VERSION - serving static website - TEMPLATES - layouts
 const express = require("express");
 
 const path = require("path");
@@ -8,8 +8,12 @@ const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 
+app.set("view engine", "hbs"); // .set para configurar el view engine -hbs-
+app.set("views", path.join(__dirname, "views")); // set para decir en q carpeta están las views, se usa path y join
+
 const PORT = 4246;
 
+// test middleware
 app.use((req, res, next) => {
   const start = Date.now();
   next(); //sin la llamada a next() se interrumpe el flujo hacia el endpoint
@@ -19,11 +23,15 @@ app.use((req, res, next) => {
 });
 
 app.use("/site", express.static(path.join(__dirname, "public"))); //hosting files, en este caso un html
-
 app.use(express.json()); //parsea los json entrantes
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "papita pa el loro",
+    caption: "aaaaaaaaaaaaa q frioo locoo",
+  });
+}); //se renderiza el template de hbs en la carpeta views con estas propiedades
 app.use("/friends", friendsRouter); //"mounting" de friends router
-
 app.use("/messages", messagesRouter);
 
 app.listen(PORT, () => {
