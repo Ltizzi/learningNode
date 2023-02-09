8e596a755fb90c3e85e2934bb4d7af224f08eedb
ltizzi <terlizzileonardo@gmail.com>
2023-02-09 19:02:43 -0300

    added endpoints

--- 8 - Security - Auth/server.js
+++ 8 - Security - Auth/server.js
@@ -2,12 +2,40 @@ const fs = require("fs");
 const path = require("path");
 const https = require("https");
 const express = require("express");
+const helmet = require("helmet");
+require("dotenv").config();
 
-const PORT = 3000;
+const PORT = process.env.PORT;
+
+const config = {
+  CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+};
 
 const app = express();
 
-app.get("/secret", (req, res) => {
+app.use(helmet()); //right up the top
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; //TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in!",
+    });
+  }
+  next();
+}
+//pre auth
+app.get("/auth/google", (req, res) => {});
+
+//post auth
+app.get("/auth/google/callback", (req, res) => {});
+
+//generic logout endpoint
+app.get("/auth/logout", (req, res) => {});
+
+//se puede pasar middlewared en los endpoints y funcionan en secuencia, se pueden agregar varios
+app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
 
